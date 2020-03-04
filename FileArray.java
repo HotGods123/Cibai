315f2baa090ea1984e16d4e18ea965321545a41f
Nicholas <nicholas@mac-air-009.cs.iastate.edu>
2020-03-04 14:28:34 -0600

    Done

--- FileArray.java
+++ FileArray.java
@@ -3,38 +3,42 @@ import java.util.*;
 
 public class FileArray 
 {
-	public static String fileName;
-	public static int content;
-	
-	public static void writeArray(String fn, int c) 
+
+	public static void writeArray(String fn, int[] c) throws FileNotFoundException 
 	{
-		fileName = fn;
-		content = c;
+		PrintWriter write = new PrintWriter(fn); 
+
+		for(int num : c)
+			write.println(num);   
+
+		write.close(); 
 	}
-	
-	public void readArray(String fn, int c)
+
+	public void readArray(String fn, int[] c) throws FileNotFoundException
 	{
-		fileName = fn;
-		content = c;
+		File file = new File(fn);
+		Scanner scan = new Scanner(file);
+		int i = 0;
+		
+		while(scan.hasNext()) {
+			c[i] = Integer.parseInt(scan.next());
+			i++;
+		}
+		
+		scan.close();
 	}
-	
+
 	public static void main(String[] args) throws IOException
 	{
-		ArrayList<Integer> contents = new ArrayList<>();
-			contents.add(12345);
-			contents.add(67890);
-		for(int i = 0; i <= contents.size(); i++)
-		{
-			System.out.print(content);
-		}
-		//try 
-		//{
-			//FileWriter file1 = new FileWriter(fileName);
-		//}
+		int nums [] = {1,2,3,4,5,6,7,8,9,10};
+		String fn = "testing.txt";
+		
+		writeArray(fn, nums);
+		
+		int newNums[] = new int[nums.length];
+		FileArray fa = new FileArray();
+		fa.readArray(fn, newNums);
+		for(int num :  newNums)
+			System.out.println(num);
 	}
-	
-	
-	
-	
-	
 }
